41c2fc72dd8fcbf9a18bb3b74e2c473d2a204fbb
Sam Broten <brotensam@gmail.com>
2023-01-30 20:34:38 -0600

    added react paper and homr component

--- App.tsx
+++ App.tsx
@@ -1,12 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import { Provider as PaperProvider } from 'react-native-paper';
+import Home from './components/Home';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <PaperProvider>
+      <View style={styles.container}>
+        <Home />
+        <StatusBar style="auto" />
+      </View>
+    </PaperProvider>
   );
 }
 
---
+++ components/Home.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { Text } from 'react-native-paper';
+
+const Home = () => {
+  return (
+    <Text variant="displayLarge">
+        FuturePrompt
+    </Text>
+  )
+}
+
+Home.propTypes = {}
+
+export default Home
