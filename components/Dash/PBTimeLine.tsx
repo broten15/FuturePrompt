5301d94ef559395442152e3c8145903918bfa61f
Yucheng Zhang <yuchengzhang.b@gmail.com>
2023-04-09 15:41:45 -0500

    timeline test

--- components/Dash/PBTimeLine.tsx
+++ components/Dash/PBTimeLine.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import Timeline from 'react-native-timeline-flatlist'
 
 const styles = StyleSheet.create({
   container: {
@@ -33,33 +34,42 @@ const styles = StyleSheet.create({
 const PBTimeLine = (props) => {
   const {promptBoards, navigation} = props;
 
+  const data = promptBoards.map((pb)=> {
+    {time: {pb.receiveDate}, title: {pb.name}, description: {pb.description}}},
+    {time: '10:45', title: 'Event 2', description: 'Event 2 Description'},
+    {time: '12:00', title: 'Event 3', description: 'Event 3 Description'},
+    {time: '14:00', title: 'Event 4', description: 'Event 4 Description'},
+    {time: '16:30', title: 'Event 5', description: 'Event 5 Description'},
+  }
   return (
-    <View>
-      {promptBoards.map((pb, index) => (
-        <TouchableOpacity 
-          key={pb.name + index} 
-          onPress={() => {
-            const today = new Date();
-            const receiveDate = new Date(pb.receiveDate);
-            if (receiveDate <= today) {
-              navigation.navigate('PBView', {
-                promptBoard: pb,
-              });
-            }
-          }}
-        >
-          <View 
-            style={styles.PBEntry}
-            
+    <><Timeline
+      data={data} />
+      <View>
+        {promptBoards.map((pb, index) => (
+          <TouchableOpacity
+            key={pb.name + index}
+            onPress={() => {
+              const today = new Date();
+              const receiveDate = new Date(pb.receiveDate);
+              if (receiveDate <= today) {
+                navigation.navigate('PBView', {
+                  promptBoard: pb,
+                });
+              }
+            } }
           >
-            <Text style={styles.entryName}>{pb.name}</Text>
-            <Text>Created on {pb.createDate}</Text>
-            <Text>Reveive on {pb.receiveDate}</Text>
-          </View>
-        </TouchableOpacity>
-      ))}
-    </View>
-  );
-}
+            <View
+              style={styles.PBEntry}
 
-export default PBTimeLine;
+            >
+              <Text style={styles.entryName}>{pb.name}</Text>
+              <Text>Created on {pb.createDate}</Text>
+              <Text>Reveive on {pb.receiveDate}</Text>
+            </View>
+          </TouchableOpacity>
+        ))}
+      </View></>
+  );
+  }
+
+  export default PBTimeLine;
